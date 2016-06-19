6f6dcaea533dd356f484e1275ef392d03be7acf7
alligator-io <markus@sonium.org>
2016-06-19 04:52:11 +0200

    remove console.log from browser/connect.js

--- test/browser/connect.js
+++ test/browser/connect.js
@@ -7,7 +7,6 @@ test('ws:connect',function(t){
 t.plan(2)
 $.get('/port',function(port){
   network.connect('ws://localhost:'+port,function(connection){
-    console.log(connection)
     _(
       connection,
       goodbye({source:_('hello'),sink:_.drain(function(d){
