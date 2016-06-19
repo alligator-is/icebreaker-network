f8bf4e80545c11ea3f328e5c57c504c62bd40c9b
alligator-io <markus@sonium.org>
2016-06-19 13:14:32 +0200

    remove jquery from test cases

--- test/browser/connect.js
+++ test/browser/connect.js
@@ -3,22 +3,21 @@ var goodbye = require('pull-goodbye')
 var network = require('../../browser.js')
 var _ = icebreaker
 
-test('ws:connect',function(t){
-t.plan(2)
-$.get('/port',function(port){
-  network.connect('wss://echo.websocket.org',function(connection){
+test('ws:connect', function (t) {
+  t.plan(2)
+  network.connect('wss://echo.websocket.org', function (connection) {
     _(
       connection,
-      goodbye({source:_('hello'),sink:_.drain(function(d){
-        t.equals(d,'hello')
-      },function(err){
-        console.log(err);
-        t.notOk(err)
-      })},'GoodBye'),
+      goodbye({
+        source: _('hello'),
+        sink: _.drain(function (d) {
+          t.equals(d, 'hello')
+        }, function (err) {
+          console.log(err);
+          t.notOk(err)
+        })
+      }, 'GoodBye'),
       connection
     )
-    })
   })
 })
-
-
