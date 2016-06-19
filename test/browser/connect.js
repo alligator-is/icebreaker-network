b24f00336cb5c95690eb83e717c08fef4e8ada31
alligator-io <markus@sonium.org>
2016-06-19 12:09:06 +0200

    Update browser test

--- test/browser/connect.js
+++ test/browser/connect.js
@@ -6,12 +6,16 @@ var _ = icebreaker
 test('ws:connect',function(t){
 t.plan(2)
 $.get('/port',function(port){
+  console.log(port)
   network.connect('ws://localhost:'+port,function(connection){
+    console.log('connection')
     _(
       connection,
       goodbye({source:_('hello'),sink:_.drain(function(d){
+        console.log('data');
         t.equals(d,'world')
       },function(err){
+        console.log(err);
         t.notOk(err)
       })},'GoodBye'),
       connection
