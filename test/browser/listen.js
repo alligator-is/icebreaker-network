154bdb370f62eaa26f6428670ed02baa6884b6fb
alligator-io <markus@sonium.org>
2016-06-19 12:16:37 +0200

    Update listen for browser test

--- test/browser/listen.js
+++ test/browser/listen.js
@@ -1,8 +1,14 @@
 var _ = require('icebreaker');
 var network = require('../../');
 var l = null;
+console.log('listen on '+process.env.ZUUL_PORT)
 var goodbye = require('pull-goodbye')
-_(l=network.listen('ws://localhost:'+process.env.ZUUL_PORT),network.on({connection:function(c){
+_(l=network.listen('ws://localhost:'+process.env.ZUUL_PORT),network.on({
+  ready:function(e){
+    console.log('ready',e);
+  },
+  connection:function(c){
+    console.log('connection')
   _(c,goodbye({ source:_('world'),sink:_.onEnd(l.end)},'GoodBye'),c)
 },
 end:function(err){
