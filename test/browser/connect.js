2fbe18abea9d263dfc8e4e8331b416be912fabb7
alligator-io <markus@sonium.org>
2019-01-24 23:22:30 +0100

    update for browser

--- test/browser/connect.js
+++ test/browser/connect.js
@@ -3,23 +3,6 @@ var goodbye = require('pull-goodbye')
 var network = require('../../browser.js')
 var _ = icebreaker
 
-test('ws:connect', function (t) {
-  t.plan(3)
-  network.connect('ws://echo.websocket.org', function (err,connection) {
-    t.notOk(err)
-    _(
-      connection,
-      goodbye({
-        source: _('hello'),
-        sink: _.drain(function (d) {
-          t.equals(d, 'hello')
-        },t.notOk)
-      }, 'GoodBye'),
-      connection
-    )
-  })
-})
-
 test('wss:connect', function (t) {
   t.plan(3)
   network.connect('wss://echo.websocket.org', function (err,connection) {
